3d8ae66883ebcd84456705aeb6b7f720fcc6735f
bytecoder8 <58306822+bytecoder8@users.noreply.github.com>
2020-01-04 11:34:50 +0500

    Add files via upload

---
+++ build/precache-manifest.78651a1d3799a401635757988c4d022c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfb7820e5a0873a66777739b4f41a0ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd252b6d602993e6bffa",
+    "url": "/static/css/main.3c352bf0.chunk.css"
+  },
+  {
+    "revision": "c76123b7afdd1e3fbe50",
+    "url": "/static/js/2.ebd06714.chunk.js"
+  },
+  {
+    "revision": "cd252b6d602993e6bffa",
+    "url": "/static/js/main.00209758.chunk.js"
+  },
+  {
+    "revision": "0d93baf50748ecf3d34c",
+    "url": "/static/js/runtime-main.8d79506e.js"
+  }
+]);
